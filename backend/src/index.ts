2fece5d94efda343fcbc1653edfea39f37331db7
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-09 21:33:48 -0400

    user model and route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import "dotenv/config";
 
+mongoose.connect(process.env.MONGO_CONNECT as string);
+
 const app = express();
+let PORT = 3303;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +16,6 @@ app.use(cors());
 app.get("/api/test", async (req: Request, res: Response) => {
     res.json({ message: "Endpoint test.." });
 });
-
-let PORT = 3303;
 
 app.listen(3303, () => {
     console.log(`SERVER HAS LAUNCHED ON PORT :: ${PORT}`);
