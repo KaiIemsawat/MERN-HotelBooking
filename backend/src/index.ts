b91112fd7dfc92fd6b1b0189fea16f81f9079482
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-10 22:45:47 -0400

    auth route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGO_CONNECT as string);
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(3303, () => {
