c936563c79cf05e8619542f83730c3432b9ecab1
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-14 18:25:31 -0400

    signin toast redirect

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,4 +1,5 @@
 import { RegisterFromData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // to use '.env' from frontend
 
 export const register = async (formData: RegisterFromData) => {
@@ -16,6 +17,25 @@ export const register = async (formData: RegisterFromData) => {
   if (!response.ok) {
     throw new Error(responseBody.message);
   }
+};
+
+export const signIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const body = await response.json();
+
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+
+  return body;
 };
 
 export const validateToken = async () => {
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,4 +1,9 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type SignInFormData = {
   email: string;
@@ -6,12 +11,31 @@ export type SignInFormData = {
 };
 
 const SignIn = () => {
+  const { showToast } = useAppContext();
+  const navigate = useNavigate();
+
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm<SignInFormData>();
+
+  const mutation = useMutation(apiClient.signIn, {
+    onSuccess: async () => {
+      showToast({ message: "Sign in successful", type: "SUCCESS" });
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold">Sign In</h2>
 
       <label className="flex-1 text-sm font-bold text-gray-700">
@@ -42,6 +66,15 @@ const SignIn = () => {
           <span className="text-red-500">{errors.password.message}</span>
         )}
       </label>
+
+      <span>
+        <button
+          type="submit"
+          className="rounded-lg bg-blue-600 p-2 px-3 py-1 text-xl font-bold text-white duration-300 hover:bg-blue-100 hover:text-blue-400 hover:shadow-lg"
+        >
+          Login
+        </button>
+      </span>
     </form>
   );
 };
