34a5896680b54a109b0bdf583b41d5a6be5ae4c7
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-13 20:10:18 -0400

    cookie/token part 1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
@@ -9,11 +10,17 @@ import authRoutes from "./routes/auth";
 mongoose.connect(process.env.MONGO_CONNECT as string);
 
 const app = express();
+app.use(cookieParser());
 let PORT = process.env.PORT || 3301;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
